6e1a4ab3aefb0e02284aa70dc9ba0c181b9cb42c
Svetozar Pavlovic <s.pavlovic@levi9.com>
2020-04-19 18:04:54 +0200

    feat: Add avatar funs

--- app/src/main/java/com/example/chatapp/CreateUserActivity.kt
+++ app/src/main/java/com/example/chatapp/CreateUserActivity.kt
@@ -1,10 +1,16 @@
 package com.example.chatapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.Random
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,21 +18,40 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
 
-    fun generateUserAvatar(view: View){
+    fun generateUserAvatar(view: View) {
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
+
+        if (color == 0) {
+            userAvatar = "light$avatar"
+        } else {
+            userAvatar = "dark$avatar"
+        }
+
+        val resId = resources.getIdentifier(userAvatar, "drawable", packageName)
+        ivCUAvatar.setImageResource(resId)
+    }
+
+    fun generateBackColor(view: View) {
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+
+        ivCUAvatar.setBackgroundColor(Color.rgb(r,g,b))
+
+        val savedR = r.toDouble() / 255
+        val savedG = g.toDouble() / 255
+        val savedB = b.toDouble() / 255
+
+        avatarColor = "[$savedR, $savedG, $savedB, 1]"
 
     }
 
-    fun generateBackColor(view: View){
+    fun createUser(view: View) {
 
     }
-
-    fun createUser(view: View){
-
-    }
-
-
-
-
 
 
 }
