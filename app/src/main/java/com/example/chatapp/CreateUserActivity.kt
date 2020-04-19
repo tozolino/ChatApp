143f0200b0a011bf6774ee9186742acc85ce1d14
Svetozar Pavlovic <s.pavlovic@levi9.com>
2020-04-19 17:48:17 +0200

    feat: Add create user activity

---
+++ app/src/main/java/com/example/chatapp/CreateUserActivity.kt
@@ -0,0 +1,32 @@
+package com.example.chatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+
+    fun generateUserAvatar(view: View){
+
+    }
+
+    fun generateBackColor(view: View){
+
+    }
+
+    fun createUser(view: View){
+
+    }
+
+
+
+
+
+
+}
--- app/src/main/java/com/example/chatapp/LoginActivity.kt
+++ app/src/main/java/com/example/chatapp/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -19,7 +20,8 @@ class LoginActivity : AppCompatActivity() {
 
 
     fun registerBtnClicked(view: View){
-
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
     }
 
 
